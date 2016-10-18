acd7a73dbba6cc1690f0c7a09d64e1299ba60f33
fjruiz <pajarrurro@gmail.com>
2016-10-18 10:18:30 +0200

    Inicio

---
+++ app/src/main/java/es/iesnervion/fjruiz/a03_listas/Principal.java
@@ -0,0 +1,13 @@
+package es.iesnervion.fjruiz.a03_listas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
