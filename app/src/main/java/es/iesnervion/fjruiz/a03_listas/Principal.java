77a53407a4b31313f1df03ff4e6a7f1829bf2261
fjruiz <pajarrurro@gmail.com>
2016-10-20 12:17:25 +0200

    Añadidos los equipos

--- app/src/main/java/es/iesnervion/fjruiz/a03_listas/Principal.java
+++ app/src/main/java/es/iesnervion/fjruiz/a03_listas/Principal.java
@@ -11,7 +11,13 @@ public class Principal extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         LolTeam origen=new LolTeam(0,"Origen","Peaso equipo loco",R.drawable.origen,15);
-        LolTeam[] teams=new LolTeam[]{origen};
+        LolTeam tsm=new LolTeam(1,"Team Solo Mid","Meh",R.drawable.tsm,18);
+        LolTeam skt=new LolTeam(2,"SKT","Te reviento la vida tt",R.drawable.skt,99);
+        LolTeam g2=new LolTeam(3,"Gamers2","Los Newbies",R.drawable.g2,0);
+        LolTeam clg=new LolTeam(4,"Counter Logic Gaming","NPI de que equipo es",R.drawable.clg,23);
+
+
+        LolTeam[] teams=new LolTeam[]{origen,tsm,skt,skt,g2,clg};
 
 
         MyArrayAdapter a=new MyArrayAdapter(this,R.layout.fila,R.id.texto,teams);
