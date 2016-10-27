e4a04d8ab57ea55409b4f599eab2de99b168081b
fjruiz <pajarrurro@gmail.com>
2016-10-27 14:06:39 +0200

    Acabado con view holder

--- app/src/main/java/es/iesnervion/fjruiz/a03_listas/Secundaria.java
+++
@@ -1,8 +0,0 @@
-package es.iesnervion.fjruiz.a03_listas;
-
-/**
- * Created by fjruiz on 20/10/16.
- */
-
-public class Secundaria {
-}
---
+++ app/src/main/java/es/iesnervion/fjruiz/a03_listas/ViewHolder.java
@@ -0,0 +1,8 @@
+package es.iesnervion.fjruiz.a03_listas;
+
+/**
+ * Created by fjruiz on 27/10/16.
+ */
+
+public class ViewHolder {
+}
